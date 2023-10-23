f8b1abf314e2974161290a4e3780520046a1bc96
Pratik <pratik.kharad@gophygital.io>
2023-10-23 17:56:19 +0530

    changes in home controller

--- DockerApp/Controllers/HomeController.cs
+++ DockerApp/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace DockerApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AddPerson()
+        {
+            return View();
+        }
     }
 }
